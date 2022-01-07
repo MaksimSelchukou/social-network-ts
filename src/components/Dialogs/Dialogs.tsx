acfdbcdd34d72d678f41c6d9230383303180ccf4
maksimselchukou <selchukouwebdev1995@gmail.com>
2022-01-07 16:59:29 +0300

    Add new directories,add new files(...),translated js into ts

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import s from './Dialogs.module.css';
+
+export const Dialogs = () => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Tatiana
+                </div>
+                <div className={s.dialog}>
+                    Maksim
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hello
+                </div>
+                <div className={s.message}>
+                    Bye-bye
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
+
+
+export const Navbar = () => {
+    return <nav className={s.nav}>
+        <div>
+            <NavLink to="/profile" className={({isActive}) => isActive ? s.active : s.item}>Profile</NavLink>
+        </div>
+        <div>
+            <NavLink to="/dialogs" className={({isActive}) => isActive ? s.active : s.item}>Messages</NavLink>
+        </div>
+        <div>
+            <NavLink to='/news' className={({isActive}) => isActive ? s.active : s.item}>News</NavLink>
+        </div>
+        <div>
+            <NavLink to='/music' className={({isActive}) => isActive ? s.active : s.item}>Music</NavLink>
+        </div>
+        <div>
+            <NavLink to='/settings' className={({isActive}) => isActive ? s.active : s.item}>Settings</NavLink>
+        </div>
+    </nav>
+}
